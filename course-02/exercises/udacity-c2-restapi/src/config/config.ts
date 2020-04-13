089e49e7c223467e4bc8973943f539c41cba3c8f
Andres Ramos <kamusky1@yahoo.es>
2020-04-13 11:35:17 -0500

    Project Stabilization

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": process.env.AWS_REGION,
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udacity-bucket-dev"
   },
   "prod": {
     "username": "",
@@ -15,5 +15,8 @@ export const config = {
     "database": "udagram_prod",
     "host": "",
     "dialect": "postgres"
+  },
+  "jwt": {
+    "secret": process.env.JWT_SECRET
   }
 }
--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -16,14 +16,17 @@ router.get('/', async (req: Request, res: Response) => {
     res.send(items);
 });
 
-//@TODO
-//Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id', async (req: Request, res: Response) => {
+    let { id } = req.params;
+    const item = await FeedItem.findByPk(id);
+    res.send(item);
+});
 
 // update a specific resource
 router.patch('/:id', 
     requireAuth, 
     async (req: Request, res: Response) => {
-        //@TODO try it yourself
+        //TODO: try it yourself
         res.send(500).send("not implemented")
 });
 
