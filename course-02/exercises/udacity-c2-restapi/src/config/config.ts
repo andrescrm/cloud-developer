ed749e54044111e87ab42c81d25bd7b55a5fe446
Andres Ramos <kamusky1@yahoo.es>
2020-04-13 21:15:30 -0500

    Fix for profile configuration

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -6,7 +6,7 @@ export const config = {
     "host": process.env.POSTGRES_HOST,
     "dialect": "postgres",
     "aws_region": process.env.AWS_REGION,
-    "aws_profile": "default",
+    "aws_profile": process.env.AWS_PROFILE,
     "aws_media_bucket": "udacity-bucket-dev"
   },
   "prod": {
